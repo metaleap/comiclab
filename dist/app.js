8afee30cb5b2fa35539002889ab7b70883941b53
Phil Schumann <metaleap@users.noreply.github.com>
2023-09-11 13:55:51 +0200

    [autopush] «BLISS is ignorance.»

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,11 @@
-import { w2layout, w2sidebar, w2toolbar, w2utils, query } from '/w2ui/w2ui-2.0.es6.js'
+import { w2layout, w2sidebar, w2toolbar, w2utils, w2popup, query } from '/w2ui/w2ui-2.0.es6.js'
 import { config_authors } from '/config-authors.js'
 
 const appState = {
     proj: {},
     config: {},
 }
+const mainDiv = query('#main');
 
 const gui_main = {
     layout: new w2layout({
@@ -28,22 +29,30 @@ const gui_main = {
                                 { id: 'menu_cfg_reload', text: 'Reload', icon: 'fa fa-refresh', },
                             ]
                         },
+                        { type: 'break' },
+                        { type: 'button', id: 'both_reload', tooltip: 'Reload Both', icon: 'fa fa-refresh' },
+                        { type: 'button', id: 'both_save', tooltip: 'Save Both', icon: 'fa fa-save', disabled: true },
                         { type: 'spacer' },
                     ],
                     onClick(evt) {
                         switch (evt.target) {
                             case 'menu_proj:menu_proj_reload':
-                                console.log(evt)
-                                lockUnlock(true)
+                                appStateReload(true, false)
                                 break
                             case 'menu_proj:menu_proj_save':
-                                lockUnlock(true)
+                                appStateSave(true, false)
                                 break
                             case 'menu_cfg:menu_cfg_reload':
-                                lockUnlock(true)
+                                appStateReload(false, true)
                                 break
                             case 'menu_cfg:menu_cfg_save':
-                                lockUnlock(true)
+                                appStateSave(false, true)
+                                break
+                            case 'both_reload':
+                                appStateReload(true, true)
+                                break
+                            case 'both_save':
+                                appStateSave(true, true)
                                 break
                         }
                     }
@@ -68,15 +77,58 @@ const gui_main = {
                     { id: 'cfg_authors', text: 'Authors', icon: 'fa fa-vcard', count: 11 }
                 ],
             },
+            {
+                id: 'log', text: 'Log', group: true, expanded: true, groupShowHide: false, nodes: [],
+            },
         ],
     }),
 }
 
+export function logErr(err) { logMsg(true, err) }
+export function logInfo(msg) { logMsg(false, msg) }
+export function logMsg(isErr, msg) {
+    const now = new Date()
+    logMsg.mostRecentItem = gui_main.sidebar.insert('log', logMsg.mostRecentItem,
+        {
+            id: 'log_item_' + now.getTime(), count: now.toLocaleTimeString(undefined, {
+                hour12: false,
+            }), text: msg, tooltip: msg, icon: 'fa ' + (isErr ? 'fa-info-circle' : 'fa-exclamation-triangle'),
+            onClick(evt) {
+                w2popup.open({ title: now.toTimeString(), text: msg })
+            }
+        },
+    ).id
+    gui_main.sidebar.expand('log')
+}
+
 function lockUnlock(locked) {
     if (locked)
-        w2utils.lock(query('#main'), { spinner: true })
+        w2utils.lock(mainDiv, { spinner: true })
     else
-        w2utils.unlock(query('#main'))
+        w2utils.unlock(mainDiv)
+}
+
+function appStateReload(proj, cfg) {
+    lockUnlock(true)
+    fetch('/appState', { method: 'POST', priority: 'high' })
+        .finally(lockUnlock(false))
+        .catch(logErr)
+        .then((resp) => {
+            if (!resp.ok)
+                throw (resp.statusText)
+            return resp.json().catch(logErr)
+        }).then((latestAppState) => {
+            if (proj && latestAppState)
+                appState.proj = latestAppState.proj
+            if (cfg && latestAppState)
+                appState.config = latestAppState.config
+            if ((proj || cfg) && latestAppState)
+                mainDiv.trigger('reloaded', { 'proj': proj, 'cfg': cfg })
+        })
+}
+
+function appStateSave(proj, cfg) {
+    lockUnlock(true)
 }
 
 // gui_main.sidebar.on('*', (evt) => { console.log('gui_main.sidebar', evt) })
