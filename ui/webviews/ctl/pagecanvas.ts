3a729c4da6c802e331908d62bb016fab4003d771
Phil Schumann <metaleap@users.noreply.github.com>
2023-09-22 09:49:45 +0200

    [autopush] «You will be surrounded by luxury.»

--- ui/webviews/ctl/pagecanvas.ts
+++ ui/webviews/ctl/pagecanvas.ts
@@ -2,55 +2,30 @@ import van, { ChildDom } from '../vanjs/van-1.2.1.js'
 import * as utils from '../utils.js'
 import * as º from '../_º.js'
 
-const html = van.tags
 const svg = van.tagsNS("http://www.w3.org/2000/svg")
 
 export type PageCanvas = {
-    page: º.Page
-    domId: string
-    domStyle: { [_: string]: string }
     dom?: HTMLElement & SVGElement
-    onReloaded(page: º.Page): void
-    onUserModified: () => void
+    selPanelIdx?: number,
 }
 
 export function create(domId: string, page: º.Page, onUserModified: (_: º.Page) => void, dbg: (...msg: any[]) => void): PageCanvas {
     const page_size = º.pageSizeMm(page)
-    const ret: PageCanvas = {
-        domId: domId,
-        page: page,
-        domStyle: { 'width': `${page_size.wMm}mm`, 'height': `${page_size.hMm}mm`, 'background-color': '#fff' },
-        onUserModified: () =>
-            onUserModified(ret.page),
-        onReloaded: (newPage: º.Page) => {
-            ret.page = newPage
-            render(ret)
-        },
-    }
-    render(ret)
-    return ret
-}
-
-function render(me: PageCanvas) {
-    const old_dom = me.dom
-    me.dom = undefined
-
-    const page_size = º.pageSizeMm(me.page)
-    let sel_panel_idx: number | undefined = undefined
+    const it: PageCanvas = {}
 
     const panels: Element[] = []
-    for (let pidx = 0; pidx < me.page.panels.length; pidx++) {
-        const panel = me.page.panels[pidx]
+    for (let pidx = 0; pidx < page.panels.length; pidx++) {
+        const panel = page.panels[pidx]
         const rect = svg.rect({
             'class': 'panel', 'id': 'panel_' + pidx, 'data-pidx': pidx, 'tabindex': 2,
             'x': `${panel.x}mm`, 'y': `${panel.y}mm`, 'width': `${panel.w}mm`, 'height': `${panel.h}mm`,
             'onfocus': (evt: MouseEvent) => {
                 evt.stopPropagation()
-                sel_panel_idx = pidx
+                it.selPanelIdx = pidx
             },
             'onblur': (evt: MouseEvent) => {
                 evt.stopPropagation()
-                sel_panel_idx = undefined
+                it.selPanelIdx = undefined
             },
             'onkeydown': (evt: KeyboardEvent) => {
                 switch (evt.key) {
@@ -67,7 +42,7 @@ function render(me: PageCanvas) {
                             panel.y = panel.y + ((evt.shiftKey ? 10 : 1) * factor)
                             rect.setAttribute('y', panel.y + 'mm')
                         }
-                        me.onUserModified()
+                        onUserModified(page)
                         break
                 }
             }
@@ -75,13 +50,12 @@ function render(me: PageCanvas) {
         panels.push(rect)
     }
 
-    const dom = svg.svg({
-        'id': me.domId, 'tabindex': 1,
+    const dom_style = { 'width': `${page_size.wMm}mm`, 'height': `${page_size.hMm}mm`, 'background-color': '#fff' }
+    it.dom = svg.svg({
+        'id': domId, 'tabindex': 1,
         'width': `${page_size.wMm}mm`,
         'height': `${page_size.hMm}mm`,
-        'style': utils.dictToArr(me.domStyle, (k, v) => k + ':' + v).join(';'),
-    }, ...panels)
-    if (old_dom)
-        old_dom.replaceWith(dom)
-    me.dom = dom as any
+        'style': utils.dictToArr(dom_style, (k, v) => k + ':' + v).join(';'),
+    }, ...panels) as any
+    return it
 }
---
+++ ui/webviews/ctl/panelwidget.ts
@@ -0,0 +1,9 @@
+import van, { ChildDom } from '../vanjs/van-1.2.1.js'
+import * as utils from '../utils.js'
+import * as º from '../_º.js'
+
+const html = van.tags
+
+export function create(domId: string) {
+
+}
